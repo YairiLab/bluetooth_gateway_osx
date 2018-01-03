6cafc03b55bed0560e17047b34f50d495a7d9fc3
Phil K. Owen <hiroki@yairilab.net>
2018-01-03 16:23:56 +0900

    プロジェクト初期化

---
+++ BluetoothGateway/BluetoothGateway/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BluetoothGateway
+//
+//  Created by OE Hi Loki on 2018/01/03.
+//  Copyright © 2018 Yairi Lab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BluetoothGateway/BluetoothGateway/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoothGateway
+//
+//  Created by OE Hi Loki on 2018/01/03.
+//  Copyright © 2018 Yairi Lab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
